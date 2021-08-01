30ef166bb4fbfeecb771a0cc40af31ed8193bf4c
lottery248 <leemeifung82@yahoo.com.hk>
2021-08-01 13:33:19 +0800

    Create scoreset13.lua
    a score list for handset 13.

---
+++ scoreset13.lua
@@ -0,0 +1,9 @@
+local scoreList13 = {
+  repBr2 = {2, 1},
+  repBr3 = {12, 4, 16, 6}, -- the first number of this array is for simple scoring system or levelled for second at [10], [16] for the third and fourth respectively. if there is only two numbers, then it's combined.
+  repBr4 = {32, 10, 64, 12},
+  qrepBr3 = {16, 6, 32, 10},
+  qrepBr4 = {256, 18, 512, 36},
+  
+
+}
