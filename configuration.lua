5fdf80e508a6abded364975c68da2ede523ea4f6
lottery248 <leemeifung82@yahoo.com.hk>
2021-08-05 14:05:26 +0800

    Update configuration.lua
    added descriptions for each configuration. planned to create GUI-based configuration.

--- configuration.lua
+++ configuration.lua
@@ -1,4 +1,4 @@
-local modules = {}
-local rulesetList = {}
-local handset = {}
-local typeChanges = {}
+local modules = {} -- modules that will be used. note that not all modules are automated like [4tp.] which requires manual execution.
+local rulesetList = {} -- consider as default if left blank.
+local handset = {} -- 13/16/17. how many cards in the concealed hand without any exposed melds or concealed quad whenever it's not your turn.
+local typeChanges = {} -- in case players want to modify the score reference, typeChanges will have a type and corresponding score in array. for example, repBr4[1] = 200 will change the simple score used in cardset 10 to 200.
