c9366a559319de43139031d20b9405c33388e93a
lottery248 <leemeifung82@yahoo.com.hk>
2021-08-12 10:44:52 +0800

    Update overview.lua
    updated the rough object creation.

--- overview.lua
+++ overview.lua
@@ -1,1 +1,15 @@
+-- make a list of attributes when the hand is detected.
+-- remember, make it an object so that it won't end up complicated codes.
 
+
+function completeHands:new (list, concealedHand, exposedHand, handset)
+ list = list or {}
+ setmetatable(list, self)
+ self.handset = handset
+ if exposedHand == {} then
+    self.hasExposure = true
+    self.exposedMeld = exposedHand
+ else self.hasExposure = false end
+ self.concealedHand = concealedHand
+  
+end
