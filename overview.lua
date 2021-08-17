72e53669c39ec57190abfa4beb5aad8ddc98c1d6
lottery248 <leemeifung82@yahoo.com.hk>
2021-08-17 15:11:22 +0800

    Update overview.lua

--- overview.lua
+++ overview.lua
@@ -1,5 +1,6 @@
 -- make a list of attributes when the hand is detected.
 -- remember, make it an object so that it won't end up complicated codes.
+-- this code should be placed in global area.
 
 
 function completeHands:new (list, concealedHand, exposedHand, handset)
@@ -13,3 +14,6 @@ function completeHands:new (list, concealedHand, exposedHand, handset)
  self.concealedHand = concealedHand
   
 end
+
+-- add detector zones.
+
