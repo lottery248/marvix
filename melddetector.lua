243a35626c6ab5fc8e76eb5719b835734b453e3b
lottery248 <leemeifung82@yahoo.com.hk>
2021-11-20 18:15:55 +0800

    Update and rename detector.lua to melddetector.lua
    i will separate the detector into three parts, one is the individual melds, one is the concealed hand, and one is the key and bonus cards.
    where this is for exposed melds.

--- melddetector.lua
+++ melddetector.lua
@@ -3,25 +3,6 @@
 
 local colourList = {"a", "b", "c", "d"} -- four colours, respectively red, yellow, green, blue.
 local numberList = {'00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17'} -- 17 is reserved for WL.
-local concealedHand = {} -- which is a zone the respective player has.
-
--- insert codes for detecting the cards below.
-local hands = {}
-local concealedHand = {}
-local exposedHand = {}
-local sortedConcealed = {}
-local handset = nil -- check which handset is set.
-
--- define players' hands. trigger calculation is a way to clean up the previous score calculation.
-
-function triggerCalculation()
-  hands = {} -- clear the hand list before starting to detect a new set of cards.
-  concealedHand = {}
-  exposedHand = {}
-  sortedConcealed = {}
-  handset = -- must be either 13, 16 or 17 in the current. 
-  sortConcealedHands()
-end
 
 
 -- insert codes to sort the cards in the concealed hand, after the concealed hand returns the list of cards. except for handset 17, always check the pair.
@@ -40,15 +21,32 @@ function sortConcealedHands(objectTarget)
   objectTarget.concealedHand = concealedHand
 end 
 
--- rework the code below as object-oriented.
-function handStatus:new (handsCreated, concealedHand, exposedHand, handset)
-  handsCreated = handsCreated or {}
-  setmetatable (handsCreated, self)
-  self.__index = self
-  self.handset = handset
-  self.concealed = concealedHand
-  self.exposed = exposedHand
-  return handsCreated
+-- remake this function here.
+
+function sendMeldInfo()
+local sortedMeld = {}
+local colours = 0
+local isSequence = false
+local isTriplet = false
+local case0 = "there are invalid exposed melds."
+
+ for i, obj in ipairs(meldZone.getObjects()) do
+   local cardCache = {}
+   local cardSepr = {}
+   cardCache = obj[i].getName()
+   for g in (cardCache.." "):gmatch("(.-)".." ") do
+      table.insert(cardSepr, g)
+    end
+  table.insert(sortedMeld, {cardSepr[1], cardSepr[2]})
+  end
+ table.sort(sortedMeld)
+ end
+
+local sortedMeldCount = 0
+for _ in pairs(sortedMeld) do sortedMeldCount = sortedMeldCount + 1 end
+if (not (sortedMeldCount == 3 or sortedMeldCount == 4)) then error case0 end
+
+-- first, check if the meld is in the same colour.
+for i in pairs(sortedMeld) do
+  if (not sortedMeld[i][1] == sortedMeld[i+1][1]) then colours = colours + 1
 end
-
-
